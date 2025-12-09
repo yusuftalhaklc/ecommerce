d72599d99d4a8a8efa73190ad1fb10dafa67136f
yusuf <yusuftalhaklc@gmail.com>
2025-12-09 12:20:01 +0300

    add AppDbContext

---
+++ Infrastucture/ecommerce.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,31 @@
+using ecommerce.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace ecommerce.Persistence.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<AppUserProfile> AppUserProfiles { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductAttribute> ProductAttributes { get; set; }
+        public DbSet<ProductAttributeValue> ProductAttributeValues { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Shipper> Shippers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+
